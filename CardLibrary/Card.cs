6adb8de104cbceee8680ea0fe21ee732e0fb4e07
Sunho <Sunho@DESKTOP-7OV12KD>
2020-04-01 21:09:04 -0400

    card library without service attributes

--- CardLibrary/Card.cs
+++ CardLibrary/Card.cs
@@ -12,7 +12,30 @@ using System.Threading.Tasks;
 
 namespace CardLibrary
 {
+    [Serializable]
     public class Card
     {
+        //Define possible values for suit and rank
+        public enum SuitID { Clubs, Diamonds, Hearts, Spades };
+        public enum RankID { Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two };
+
+        // Public methods & properties
+
+        public SuitID Suit { get; private set; }
+
+        public RankID Rank { get; private set; }
+
+        public override string ToString()
+        {
+            return Rank.ToString() + " of " + Suit.ToString();
+        }
+
+        // Constructor
+
+        internal Card(SuitID s, RankID r)
+        {
+            Suit = s;
+            Rank = r;
+        }
     }
 }
