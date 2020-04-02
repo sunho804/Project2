dffaf04432a8005ea10feb0bf02892f156a8955b
Sunho <Sunho@DESKTOP-7OV12KD>
2020-04-01 20:48:17 -0400

    Add project files.

---
+++ CardLibrary/Card.cs
@@ -0,0 +1,18 @@
+﻿/***
+ * Author: Sunho Jung, Sue Koh
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardLibrary
+{
+    public class Card
+    {
+    }
+}
