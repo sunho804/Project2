d22b0f7330599129d5ba066f95202cb7df98b451
sue1007 <37780021+sue1007@users.noreply.github.com>
2020-04-04 17:38:55 -0400

    Add files via upload

---
+++ Card.cs
@@ -0,0 +1,44 @@
+﻿/***
+ * Author: Sunho Jung, Sue Koh
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization; // WCF Data Contract types
+
+
+namespace CardLibrary
+{
+    //   [Serializable]
+    [DataContract]
+    public class Card
+    {
+        //Define possible values for suit and rank
+        public enum SuitID { Clubs, Diamonds, Hearts, Spades };
+        public enum RankID { Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two };
+
+        // Public methods & properties
+        [DataMember]
+        public SuitID Suit { get; private set; }
+        [DataMember]
+        public RankID Rank { get; private set; }
+
+        public override string ToString()
+        {
+            return Rank.ToString() + " of " + Suit.ToString();
+        }
+
+        // Constructor
+
+        internal Card(SuitID s, RankID r)
+        {
+            Suit = s;
+            Rank = r;
+        }
+    }
+}
